e47e7f55c98749c4806bff6a9909753acbc6b6ff
Ashiamd <ashiamd@foxmail.com>
2021-02-04 18:04:54 +0800

    定义POJO

---
+++ HotItemsAnalysis/src/main/java/beans/ItemViewCount.java
@@ -0,0 +1,53 @@
+package beans;
+
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/4 6:00 PM
+ */
+public class ItemViewCount {
+    private Long itemId;
+    private Long windowEnd;
+    private Long count;
+
+    @Override
+    public String toString() {
+        return "ItemViewCount{" +
+                "itemId=" + itemId +
+                ", windowEnd=" + windowEnd +
+                ", count=" + count +
+                '}';
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getWindowEnd() {
+        return windowEnd;
+    }
+
+    public void setWindowEnd(Long windowEnd) {
+        this.windowEnd = windowEnd;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public ItemViewCount(Long itemId, Long windowEnd, Long count) {
+        this.itemId = itemId;
+        this.windowEnd = windowEnd;
+        this.count = count;
+    }
+
+    public ItemViewCount() {
+    }
+}
