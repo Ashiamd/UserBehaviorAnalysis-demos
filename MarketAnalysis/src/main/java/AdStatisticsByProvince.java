bb825909c9e1213d04465e55279c5ece2fb9ba29
Ashiamd <ashiamd@foxmail.com>
2021-02-06 01:33:04 +0800

    市场营销分析——页面广告统计 -- 代码2-点击异常行为黑名单过滤

--- MarketAnalysis/src/main/java/AdStatisticsByProvince.java
+++ MarketAnalysis/src/main/java/AdStatisticsByProvince.java
@@ -1,8 +1,17 @@
 import beans.AdClickEvent;
 import beans.AdCountViewByProvince;
+import beans.BlackListUserWarning;
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
@@ -10,10 +19,11 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter;
 import org.apache.flink.util.Collector;
-
+import org.apache.flink.util.OutputTag;
 
 import java.net.URL;
 import java.sql.Timestamp;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -42,15 +52,28 @@ public class AdStatisticsByProvince {
                         }
                 ));
 
-        // 2. 基于省份分组，开窗聚合
-        DataStream<AdCountViewByProvince> adCountStream = adClickEventDataStream
+        // 2. 对同一个用户点击同一个广告的行为进行检测报警
+        SingleOutputStreamOperator<AdClickEvent> filterAdClickStream = adClickEventDataStream
+                .keyBy(new KeySelector<AdClickEvent, Tuple2<Long, Long>>() {
+                    @Override
+                    public Tuple2<Long, Long> getKey(AdClickEvent value) throws Exception {
+                        return new Tuple2<>(value.getUserId(), value.getAdId());
+                    }
+                })
+                .process(new FilterBlackListUser(100));
+
+        // 3. 基于省份分组，开窗聚合
+        DataStream<AdCountViewByProvince> adCountResultStream = filterAdClickStream
                 .keyBy(AdClickEvent::getProvince)
                 // 定义滑窗,5min输出一次
                 .window(SlidingEventTimeWindows.of(Time.hours(1), Time.minutes(5)))
                 .aggregate(new AdCountAgg(), new AdCountResult());
 
 
-        adCountStream.print();
+        adCountResultStream.print();
+        filterAdClickStream
+                .getSideOutput(new OutputTag<BlackListUserWarning>("blacklist"){})
+        .print("blacklist-user");
 
         env.execute("ad count by province job");
     }
@@ -86,5 +109,77 @@ public class AdStatisticsByProvince {
             Long count = input.iterator().next();
             out.collect(new AdCountViewByProvince(province, windowEnd, count));
         }
+    }
+
+    // 实现自定义处理函数
+    public static class FilterBlackListUser extends KeyedProcessFunction<Tuple2<Long, Long>, AdClickEvent, AdClickEvent> {
+
+        // 定义属性：点击次数上线
+        private Integer countUpperBound;
+
+        public FilterBlackListUser(Integer countUpperBound) {
+            this.countUpperBound = countUpperBound;
+        }
+
+        // 定义状态，保存当前用户对某一广告的点击次数
+        ValueState<Long> countState;
+        // 定义一个标志状态，保存当前用户是否已经被发送到了黑名单里
+        ValueState<Boolean> isSentState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            countState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("ad-count", Long.class));
+            isSentState = getRuntimeContext().getState(new ValueStateDescriptor<Boolean>("is-sent", Boolean.class));
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<AdClickEvent> out) throws Exception {
+            // 清空所有状态
+            countState.clear();
+            isSentState.clear();
+        }
+
+        @Override
+        public void processElement(AdClickEvent value, Context ctx, Collector<AdClickEvent> out) throws Exception {
+            // 判断当前用户对同一广告的点击次数，如果不够上限，该count加1正常输出；
+            // 如果到达上限，直接过滤掉，并侧输出流输出黑名单报警
+
+            // 首先获取当前count值
+            Long curCount = countState.value();
+
+            Boolean isSent = isSentState.value();
+
+            if(null == curCount){
+                curCount = 0L;
+            }
+
+            if(null == isSent){
+                isSent = false;
+            }
+
+            // 1. 判断是否是第一个数据，如果是的话，注册一个第二天0点的定时器
+            if (curCount == 0) {
+                long ts = ctx.timerService().currentProcessingTime();
+                long fixedTime = DateUtils.addDays(new Date(ts), 1).getTime();
+                ctx.timerService().registerProcessingTimeTimer(fixedTime);
+            }
+
+            // 2. 判断是否报警
+            if (curCount >= countUpperBound) {
+                // 判断是否输出到黑名单过，如果没有的话就输出到侧输出流
+                if (!isSent) {
+                    isSentState.update(true);
+                    ctx.output(new OutputTag<BlackListUserWarning>("blacklist"){},
+                            new BlackListUserWarning(value.getUserId(), value.getAdId(), "click over " + countUpperBound + "times."));
+                }
+                // 不再进行下面操作
+                return;
+            }
+
+            // 如果没有返回，点击次数加1，更新状态，正常输出当前数据到主流
+            countState.update(curCount + 1);
+            out.collect(value);
+        }
+
     }
 }
