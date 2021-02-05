371c46247b24797c3a06ebb4f852f258b4592156
Ashiamd <ashiamd@foxmail.com>
2021-02-06 04:15:01 +0800

    恶意登录监控-代码3-CEP代码实现

---
+++ LoginFailDetect/src/main/java/LoginFailWithCep.java
@@ -0,0 +1,83 @@
+import beans.LoginEvent;
+import beans.LoginFailWarning;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternSelectFunction;
+import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/6 3:41 AM
+ */
+public class LoginFailWithCep {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1. 从文件中读取数据
+        URL resource = LoginFail.class.getResource("/LoginLog.csv");
+        DataStream<LoginEvent> loginEventStream = env.readTextFile(resource.getPath())
+                .map(line -> {
+                    String[] fields = line.split(",");
+                    return new LoginEvent(new Long(fields[0]), fields[1], fields[2], new Long(fields[3]));
+                })
+                .assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarksAdapter.Strategy<>(
+                        new BoundedOutOfOrdernessTimestampExtractor<LoginEvent>(Time.of(200, TimeUnit.MILLISECONDS)) {
+                            @Override
+                            public long extractTimestamp(LoginEvent element) {
+                                return element.getTimestamp() * 1000L;
+                            }
+                        }
+                ));
+
+        // 1. 定义一个匹配模式
+        // firstFail -> secondFail, within 2s
+        Pattern<LoginEvent, LoginEvent> loginFailPattern = Pattern
+                .<LoginEvent>begin("firstFail").where(new SimpleCondition<LoginEvent>() {
+                    @Override
+                    public boolean filter(LoginEvent value) throws Exception {
+                        return "fail".equals(value.getLoginState());
+                    }
+                })
+                .next("secondFail").where(new SimpleCondition<LoginEvent>() {
+                    @Override
+                    public boolean filter(LoginEvent value) throws Exception {
+                        return "fail".equals(value.getLoginState());
+                    }
+                })
+                .within(Time.seconds(2));
+
+        // 2. 将匹配模式应用到数据流上，得到一个pattern stream
+        PatternStream<LoginEvent> patternStream = CEP.pattern(loginEventStream.keyBy(LoginEvent::getUserId), loginFailPattern);
+
+        // 3. 检出符合匹配条件的复杂事件，进行转换处理，得到报警信息
+        SingleOutputStreamOperator<LoginFailWarning> warningStream = patternStream.select(new LoginFailMatchDetectWarning());
+
+        warningStream.print();
+
+        env.execute("login fail detect with cep job");
+    }
+
+    // 实现自定义的PatternSelectFunction
+    public static class LoginFailMatchDetectWarning implements PatternSelectFunction<LoginEvent, LoginFailWarning> {
+        @Override
+        public LoginFailWarning select(Map<String, List<LoginEvent>> pattern) throws Exception {
+            LoginEvent firstFailEvent = pattern.get("firstFail").iterator().next();
+            LoginEvent lastFailEvent = pattern.get("secondFail").get(0);
+            return new LoginFailWarning(firstFailEvent.getUserId(), firstFailEvent.getTimestamp(), lastFailEvent.getTimestamp(), "login fail 2 times");
+        }
+    }
+}
