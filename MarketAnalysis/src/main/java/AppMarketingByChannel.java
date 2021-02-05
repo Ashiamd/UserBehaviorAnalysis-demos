29e7a23e351b25bb5a494dfd4467f86ff2dbc5ae
Ashiamd <ashiamd@foxmail.com>
2021-02-05 18:04:55 +0800

    市场营销分析——APP市场推广统计 - 具体代码实现

--- MarketAnalysis/src/main/java/AppMarketingByChannel.java
+++ MarketAnalysis/src/main/java/AppMarketingByChannel.java
@@ -1,11 +1,27 @@
+import beans.ChannelPromotionCount;
 import beans.MarketingUserBehavior;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter;
+import org.apache.flink.util.Collector;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * @author : Ashiamd email: ashiamd@foxmail.com
@@ -17,7 +33,32 @@ public class AppMarketingByChannel {
         env.setParallelism(1);
 
         // 1. 从自定义数据源中读取数据
-        DataStream<MarketingUserBehavior> dataStream = env.addSource(new SimulatedMarketingUserBehaviorSource());
+        DataStream<MarketingUserBehavior> dataStream = env.addSource(new SimulatedMarketingUserBehaviorSource())
+                .assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarksAdapter.Strategy<>(
+                        new BoundedOutOfOrdernessTimestampExtractor<MarketingUserBehavior>(Time.of(200, TimeUnit.MILLISECONDS)) {
+                            @Override
+                            public long extractTimestamp(MarketingUserBehavior element) {
+                                return element.getTimestamp();
+                            }
+                        }
+                ));
+
+        // 2. 分渠道开窗统计
+        DataStream<ChannelPromotionCount> resultStream = dataStream
+                .filter(data -> !"UNINSTALL".equals(data.getBehavior()))
+                .keyBy(new KeySelector<MarketingUserBehavior, Tuple2<String, String>>() {
+                    @Override
+                    public Tuple2<String, String> getKey(MarketingUserBehavior value) throws Exception {
+                        return new Tuple2<>(value.getChannel(), value.getBehavior());
+                    }
+                })
+                // 定义滑窗
+                .window(SlidingEventTimeWindows.of(Time.hours(1), Time.seconds(5)))
+                .aggregate(new MarketingCountAgg(), new MarketingCountResult());
+
+        resultStream.print();
+
+        env.execute("app marketing by channel job");
 
     }
 
@@ -51,6 +92,43 @@ public class AppMarketingByChannel {
         @Override
         public void cancel() {
             running = false;
+        }
+    }
+
+    // 实现自定义的增量聚合函数
+    public static class MarketingCountAgg implements AggregateFunction<MarketingUserBehavior, Long, Long> {
+
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        @Override
+        public Long add(MarketingUserBehavior value, Long accumulator) {
+            return accumulator + 1;
+        }
+
+        @Override
+        public Long getResult(Long accumulator) {
+            return accumulator;
+        }
+
+        @Override
+        public Long merge(Long a, Long b) {
+            return a + b;
+        }
+    }
+
+    // 实现自定义的全窗口函数
+    public static class MarketingCountResult extends ProcessWindowFunction<Long, ChannelPromotionCount, Tuple2<String, String>, TimeWindow> {
+
+        @Override
+        public void process(Tuple2<String, String> stringStringTuple2, Context context, Iterable<Long> elements, Collector<ChannelPromotionCount> out) throws Exception {
+            String channel = stringStringTuple2.f0;
+            String behavior = stringStringTuple2.f1;
+            String windowEnd = new Timestamp(context.window().getEnd()).toString();
+            Long count = elements.iterator().next();
+            out.collect(new ChannelPromotionCount(channel, behavior, windowEnd, count));
         }
     }
 }
